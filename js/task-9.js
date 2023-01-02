523b107e43bb343a15d24008645936d99a83a46e
alexeytkachenko1 <alexeytkachenko1@gmail.com>
2023-01-02 21:24:44 +0200

    task 9

--- js/task-9.js
+++ js/task-9.js
@@ -26,21 +26,39 @@
 //     //     console.log('you cancel');
 //   }
 // }
-`use strict`;
-const secretNumber = Math.round(Math.random() * (100 - 1) + 1);
-// console.log(secretNumber);
-let userNumber;
-do {
-  const userInput = prompt('Please enter the number');
-  userNumber = Number.parseInt(userInput);
-  console.log(userNumber);
-  if (userNumber === secretNumber) {
-    console.log('You won!');
-  } else if (userNumber > secretNumber) {
-    console.log('Please enter the less figure');
-  } else if (userNumber < secretNumber) {
-    console.log('Please enter the more figure');
-  } else {
-    console.log('exit');
+// `use strict`;
+// const secretNumber = Math.round(Math.random() * (100 - 1) + 1);
+// // console.log(secretNumber);
+// let userNumber;
+// do {
+//   const userInput = prompt('Please enter the number');
+//   userNumber = Number.parseInt(userInput);
+//   console.log(userNumber);
+//   if (userNumber === secretNumber) {
+//     console.log('You won!');
+//   } else if (userNumber > secretNumber) {
+//     console.log('Please enter the less figure');
+//   } else if (userNumber < secretNumber) {
+//     console.log('Please enter the more figure');
+//   } else {
+//     console.log('exit');
+//   }
+// } while (secretNumber !== userNumber && !isNaN(userNumber));
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+let total = 0;
+
+for (let i = 0; i < matrix.length; i += 1) {
+  console.log('Подмассив матрицы matrix[i]: ', matrix[i]);
+
+  for (let j = 0; j < matrix[i].length; j += 1) {
+    console.log('Элемент подмассива матрицы matrix[i][j]: ', matrix[i][j]);
+    total += matrix[i][j];
   }
-} while (secretNumber !== userNumber && !isNaN(userNumber));
+}
+
+console.log(total); // 45
